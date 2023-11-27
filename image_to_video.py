a47cfbfb960b90de19caf0d1d429ded310a5b52c
Kashfi Fahim <Kashfi.Fahim@gmail.com>
2023-11-27 16:48:38 -0500

    update

--- image_to_video.py
+++ image_to_video.py
@@ -28,38 +28,28 @@ def resize_image(image, target_width, target_height):
 
     return cv2.copyMakeBorder(resized_image, top_padding, bottom_padding, left_padding, right_padding, cv2.BORDER_CONSTANT, value=[0, 0, 0])
 
-def images_to_video(input_folder, output_folder, fps=30, duration=5, target_width=1920, target_height=1080):
+def images_to_video(image_paths, output_video_file, fps=30, duration=5, target_width=1920, target_height=1080):
     """
-    Convert a set of images in a folder to a video.
+    Convert a set of images to a video.
     
     Parameters:
-    - input_folder: path to the folder containing the images
-    - output_folder: path to the output video file (e.g., 'output.mp4')
-    - fps: frames per second for the output video
-    - duration: duration in seconds for each image to be displayed
-    - target_width: width to resize images to
-    - target_height: height to resize images to
+    - image_paths: list of paths to the image files
+    - output_video_file: path to the output video file (e.g., 'output.mp4')
+    - fps, duration, target_width, target_height as before
     """
+    out = cv2.VideoWriter(output_video_file, cv2.VideoWriter_fourcc(*'XVID'), fps, (target_width, target_height))
     
-    images = [img for img in os.listdir(input_folder) if img.endswith(".png")]
-    images.sort()
-
-    out = cv2.VideoWriter(output_folder, cv2.VideoWriter_fourcc(*'XVID'), fps, (target_width, target_height))
-    
-    for image in images:
-        img_path = os.path.join(input_folder, image)
+    for img_path in image_paths:
         img = cv2.imread(img_path)
-        
         resized_img = resize_image(img, target_width, target_height)
         
-        for _ in range(fps * duration):  # This will make each image last for 'duration' seconds
+        for _ in range(fps * duration):
             out.write(resized_img)
     
     out.release()
 
+# Adapted for CrossCompute - example usage with a list of image paths
+image_paths = [str(Path('input_folder') / f'image{i}.png') for i in range(1, num_images+1)]  # num_images is the number of images you have
+output_video_file = str(Path('output_folder') / 'output_video.mp4')
 
-input_folder = Path(getenv('CROSSCOMPUTE_INPUT_FOLDER', 'default_input_path'))
-output_folder = Path(getenv('CROSSCOMPUTE_OUTPUT_FOLDER', 'default_output_path'))
-output_video_file = output_folder / 'output_video.mp4'
-
-images_to_video(input_folder, output_video_file, fps=30, duration=5)
+images_to_video(image_paths, output_video_file, fps=30, duration=5)
