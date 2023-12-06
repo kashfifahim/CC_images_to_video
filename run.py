aae01c86eae78455a93d7060213c347c84f9d57b
Kashfi Fahim <Kashfi.Fahim@gmail.com>
2023-12-06 12:35:02 -0500

    update project structure

--- run.py
+++ run.py
@@ -3,16 +3,18 @@ from pathlib import Path
 import os
 import shutil
 from image_processing import extract_images_from_zip, natural_sort_key, images_to_video
-from pdf_processing import convert_pdf_to_images
 
 if __name__ == '__main__':
+    print("Script started")
     # Setting up input and output folders
     input_folder = Path(getenv('CROSSCOMPUTE_INPUT_FOLDER', 'batches/standard/input'))
     output_folder = Path(getenv('CROSSCOMPUTE_OUTPUT_FOLDER', 'batches/standard/output'))
+    print(f"Input folder: {input_folder}, Output folder: {output_folder}")
     output_folder.mkdir(parents=True, exist_ok=True)
 
     # Creating temporary folders for processing
     temp_folder = output_folder / 'temp'
+    print(f"Temporary folder: {temp_folder}")
     temp_folder.mkdir(exist_ok=True)
     temp_video_file = temp_folder / 'temp_output_video.mp4'
 
@@ -22,8 +24,10 @@ if __name__ == '__main__':
     # Finding the first zip file in the input folder
     zip_files = list(input_folder.glob('*.zip'))
     if not zip_files:
+        print("No zip file found in the input folder")
         raise ValueError("No zip file found in the input folder")
     zip_file_path = zip_files[0]
+    print(f"Zip file found: {zip_file_path}")
 
     # Extracting images from the zip file and finding the folder with images
     images_folder = extract_images_from_zip(zip_file_path, extract_to_folder)
@@ -35,12 +39,14 @@ if __name__ == '__main__':
 
     # Setting the path for the output video file
     output_video_file = Path(output_folder / 'output_video.mp4')
-
+    print(f"Output video file will be saved as: {output_video_file}")
     # Creating the video from images
     images_to_video(image_paths, str(temp_video_file), fps=30, duration=5)
+    
 
     # Verifying the video creation and performing cleanup
     if os.path.exists(temp_video_file) and os.path.getsize(temp_video_file) > 0:
+        print(f"Video file created successfully: {temp_video_file}")
         # Move the completed video to the output folder
         shutil.move(str(temp_video_file), str(output_video_file))
         print(f"Video moved to output folder: {output_video_file}")
@@ -54,3 +60,5 @@ if __name__ == '__main__':
     else:
         print(f"Video creation failed or video file is empty: {output_video_file}")
         print(f"Video creation failed or video file is empty: {output_video_file}")
+
+    print("Script ended")
